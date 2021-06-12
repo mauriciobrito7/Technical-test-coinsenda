d6a5d3673371ce45a64ea8dd864a7cb5e408736f
mauriciopaktolus <mauricio@paktolus.com>
2021-06-12 18:58:10 -0400

    Global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import MuseoSansFont from "../assets/fonts/MuseoSans.otf";
+import MuseoSansBoldFont from "../assets/fonts/MuseoSansBold.otf";
 
 const GlobalStyles = createGlobalStyle`
   @font-face{ 
@@ -7,21 +8,57 @@ const GlobalStyles = createGlobalStyle`
     src: local('Museo Sans'), url(${MuseoSansFont}) format('truetype');
     font-weight: normal;
   }
+
+  @font-face{
+    font-family: 'Museo Sans Bold';
+    src: local('Museo Sans'), url(${MuseoSansBoldFont}) format('truetype');
+    font-weight: bold;
+  }
+
   * {
     margin: 0;
     box-sizing: border-box;
   }
+
   body {
     font-family: 'Museo Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
+      height: 100vh;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    height: 100vh;
+    border: 4px dashed gold;
+  }
+
+  a{
+	  text-decoration:none;
+  }
+
+  label, input, textarea{
+    display:block;
+  }
+
+  main{
+    padding-bottom: 4em;
+  }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Museo Sans Bold';
+    font-weight: bold;
+  }
+
+  h1 {
+    font-size: 1em * 1.7;
+  }
+
+  h2 {
+    font-size: 1em * 1.35;
+  }
+
+  h3 {
+    font-size: 1em * 1.2;
   }
   
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
 `;
 export default GlobalStyles;
