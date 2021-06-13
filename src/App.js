6983372d3cc335a781b0499f9414e9c861732e43
mauriciopaktolus <mauricio@paktolus.com>
2021-06-13 12:26:31 -0400

    Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Layout from "./components/Layout";
-import Records from "./components/Records";
+import { ScrollToTop } from "./components/ScrollToTop";
+
+//Pages
+const Balance = React.lazy(() => import("./pages/Balance"));
+const RecordDetail = React.lazy(() => import("./pages/RecordDetail"));
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <Layout>
-        <Records />
-      </Layout>
+      <Suspense fallback="Loading...">
+        <GlobalStyles />
+        <Layout>
+          <Router>
+            <ScrollToTop />
+            <Route exact path="/" component={Balance} />
+            <Route exact path="/transaction/:id" component={RecordDetail} />
+            <Switch></Switch>
+          </Router>
+        </Layout>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/Balance/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Records from "../../components/Records";
+
+function Balance() {
+  return (
+    <>
+      <Records />
+    </>
+  );
+}
+
+export default Balance;
