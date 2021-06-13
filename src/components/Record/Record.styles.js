02fdaf3c86286435dbf8ea1c0d66726ceccf122b
mauriciopaktolus <mauricio@paktolus.com>
2021-06-13 11:54:10 -0400

    fadeIn applied to cards

--- src/components/Record/Record.styles.js
+++ src/components/Record/Record.styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components/macro";
-import { device, colors, LightenDarkenColor } from "../../styles/theme";
-import { Link } from "react-router-dom";
+import { device, colors } from "../../styles/theme";
+import { fadeIn } from "../../styles/animation";
+// import { Link } from "react-router-dom";
 
 export const Card = styled.div`
   width: 100%;
-  padding: 1em 0.5em;
+  padding: 1em 1em;
   min-height: 50px;
   border-bottom: 2px solid ${colors.lightGray};
   display: flex;
@@ -12,6 +13,7 @@ export const Card = styled.div`
   flex-wrap: wrap;
   cursor: pointer;
   transition: background 0.3s ease;
+  ${fadeIn({ time: ".5s" })};
   &:nth-of-type(odd) {
     background: ${colors.hoverCard};
   }
