bb56fb3fe4b1524f795ffed6aa0becf312415c6c
mauriciopaktolus <mauricio@paktolus.com>
2021-06-12 22:35:58 -0400

    layout margin for desktop

--- src/components/Layout/Layout.style.js
+++ src/components/Layout/Layout.style.js
@@ -1,8 +1,12 @@
 import styled from "styled-components/macro";
-import { breakpoints } from "../../styles/theme";
+import { breakpoints, device } from "../../styles/theme";
 
 export const MainContent = styled.main`
   border: 1px dashed crimson;
   margin: 1em 1em;
   max-width: ${breakpoints.desktop};
+  ${device.desktop`
+    margin-right: auto;
+    margin-left: auto; 
+  `}
 `;
