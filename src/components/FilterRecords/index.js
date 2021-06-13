952975ccb1dcf37e786cc53ddb2012ceb5f5cb1f
mauriciopaktolus <mauricio@paktolus.com>
2021-06-13 17:11:49 -0400

    filter options

--- src/components/FilterRecords/index.js
+++ src/components/FilterRecords/index.js
@@ -1,13 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import { FilterContainer, Btn } from "./Filter.styles";
+import { ReactComponent as ArrowUp } from "../../assets/icons/arrowUp.svg";
+import { ReactComponent as ArrowDown } from "../../assets/icons/arrowDown.svg";
+import { ReactComponent as Exchange } from "../../assets/icons/exchange.svg";
 
 function Filter() {
+  const [filterSelected, setFilterSelected] = useState("all");
+
+  const isSelected = (option) => {
+    if (option === filterSelected) {
+      return true;
+    }
+    return false;
+  };
+
+  const selectFilter = (option) => {
+    setFilterSelected(option);
+  };
+
   return (
     <FilterContainer>
-      <Btn active={true}>Todas</Btn>
-      <Btn>Depositos</Btn>
-      <Btn>Retiros</Btn>
-      <Btn>Cambios</Btn>
+      <Btn onClick={() => selectFilter("all")} active={isSelected("all")}>
+        Todas
+      </Btn>
+      <Btn
+        onClick={() => selectFilter("deposits")}
+        active={isSelected("deposits")}
+      >
+        <ArrowUp />
+      </Btn>
+      <Btn
+        onClick={() => selectFilter("withdraws")}
+        active={isSelected("withdraws")}
+      >
+        <ArrowDown />
+      </Btn>
+      <Btn onClick={() => selectFilter("swaps")} active={isSelected("swaps")}>
+        <Exchange />
+      </Btn>
     </FilterContainer>
   );
 }
