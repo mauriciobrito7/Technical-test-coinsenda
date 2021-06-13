a797624aed60f0502a28efc07bc94f366352d4c5
mauriciopaktolus <mauricio@paktolus.com>
2021-06-13 02:03:03 -0400

    utils functions

---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export const sortArrayByDate = (array) =>
+  array.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+
+//formatterCurrency.format(value)
+export const formatterCurrency = (locales, currency) =>
+  new Intl.NumberFormat(`${locales}`, {
+    style: "currency",
+    currency: `${currency}`,
+    minimumFractionDigits: 0,
+  });
+
+export const parseJwt = (token) => {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+};
