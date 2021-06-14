b2fad9eff58b7eb2da117a9bc58ac6af8c417b0a
mauriciopaktolus <mauricio@paktolus.com>
2021-06-14 15:36:58 -0400

    activity reducer

--- src/redux/activity/activity.reducer.js
+++ src/redux/activity/activity.reducer.js
@@ -1,3 +1,5 @@
+import { normalizeData } from "../../utils/index";
+
 const INITIAL_STATE = {
   deposits: [],
   withdraws: [],
@@ -5,36 +7,46 @@ const INITIAL_STATE = {
   activities: [],
   loading: false,
   authToken:
-    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3IiOiI2MGIxZGE4NzA5ODI2ODAwNjRiMWJmMDQiLCJhdXRoX2NsaWVudF9pZCI6IjYwNjdmNWE5YmRkNzJkMDBkMTA3NjM2NSIsImlzcyI6IjYwNjdmNThlYmRkNzJkMDBkMTA3NjM1YSIsImF1ZCI6InRyYW5zYWN0aW9uLGlkZW50aXR5LGF1dGgsbm90aWZpY2F0aW9uLGluZm8sdHJhbnNhY3Rpb24sYWNjb3VudCxkZXBvc2l0LHdpdGhkcmF3LHN3YXAiLCJlbWFpbCI6InNvcG9ydGUrX3Rlc3RpbmdAY29pbnNlbmRhLmNvbSIsImxhbmd1YWdlIjoiZXMiLCJtZXRhZGF0YSI6Int9IiwianRpIjoiNjBjNzZkZDhiZjE2YzYwMDU2NDUyOGZjIiwiaWF0IjoxNjIzNjgyNTIwLCJleHAiOjE2MjM2ODI4MjB9.VYAoC19ptzGZrIPpPlyJOrOzg0BpsST4Ru9897F5Q5t-ZK-KY7kcuIPwKVEPg3rAJOH2i01J4dBAQZJ-LC8ZHA",
+    "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3IiOiI2MGIxZGE4NzA5ODI2ODAwNjRiMWJmMDQiLCJhdXRoX2NsaWVudF9pZCI6IjYwNjdmNWE5YmRkNzJkMDBkMTA3NjM2NSIsImlzcyI6IjYwNjdmNThlYmRkNzJkMDBkMTA3NjM1YSIsImF1ZCI6InRyYW5zYWN0aW9uLGlkZW50aXR5LGF1dGgsbm90aWZpY2F0aW9uLGluZm8sdHJhbnNhY3Rpb24sYWNjb3VudCxkZXBvc2l0LHdpdGhkcmF3LHN3YXAiLCJlbWFpbCI6InNvcG9ydGUrX3Rlc3RpbmdAY29pbnNlbmRhLmNvbSIsImxhbmd1YWdlIjoiZXMiLCJtZXRhZGF0YSI6Int9IiwianRpIjoiNjBjN2FhYzhiZjE2YzYwMDU2NDUyOTI5IiwiaWF0IjoxNjIzNjk4MTIwLCJleHAiOjE2MjM2OTg0MjB9.B-GodkjEFE9a0mZb0aLAKft_p-5foBmhUyASlg-sVMpB-XAuWTkkLnjJ-KjBIsz7dgs1e-9rvnWmWbn5_92Ocg",
 };
 
 const activityReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "SET_DEPOSITS":
+      const deposits = normalizeData(action.payload, "deposito");
       return {
         ...state,
-        deposits: [...state.deposits, action.payload],
+        deposits: [...state.deposits, deposits],
       };
 
     case "SET_WITHDRAWS":
+      const withdraws = normalizeData(action.payload, "retiro");
+
       return {
         ...state,
-        withdraws: [...state.withdraws, action.payload],
+        withdraws: [...state.withdraws, withdraws],
       };
 
     case "SET_SWAPS":
+      const swaps = normalizeData(action.payload, "intercambio");
       return {
         ...state,
-        swaps: [...state.swaps, action.payload],
+        swaps: [...state.swaps, swaps],
       };
     case "SET_ACTIVITIES":
-      const arrayMerge = state.deposits
-        .concat(state.withdraws)
-        .concat(state.swaps);
+      const depositsFlat = normalizeData(state.deposits, "deposito");
+      const withdrawsFlat = normalizeData(state.withdraws, "retiro");
+      const swapsFlat = normalizeData(state.swaps, "intercambio");
+
+      const arrayMerge = depositsFlat.concat(withdrawsFlat).concat(swapsFlat);
+
+      const activities = arrayMerge.sort(function (a, b) {
+        return new Date(b.created_at) - new Date(a.created_at);
+      });
 
       return {
         ...state,
-        activities: arrayMerge,
+        activities: activities,
       };
 
     case "SET_LOADING":
