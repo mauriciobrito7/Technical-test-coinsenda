524a43936733de144b03442c530bbc02ee2ee6e1
mauriciopaktolus <mauricio@paktolus.com>
2021-06-14 11:17:18 -0400

    link to home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import { HeaderContainer, Logo, Author } from "./Header.styles";
 import LogoIcon from "../../assets/icons/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <HeaderContainer>
-      <Logo>
-        <img src={LogoIcon} alt="logo" />
-      </Logo>
+      <Link to="/">
+        <Logo>
+          <img src={LogoIcon} alt="logo" />
+        </Logo>
+      </Link>
       <Author>Mauricio Brito</Author>
     </HeaderContainer>
   );
